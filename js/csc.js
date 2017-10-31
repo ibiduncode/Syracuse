1a07e8d27fad661235c34c7af0acd526cd8219af
ibiduncode <ibidunmi@yahoo.com>
2017-10-31 11:16:57 +0100

    damie

---
+++ js/csc.js
@@ -0,0 +1,22 @@
+public class { Employee
+
+String name;
+int age;
+String destination;
+double salary;
+
+//This is the constructor of the class REmployee 
+public Employee(String name){
+  this.name;
+  }
+  
+  //Assign the age of the Eployee to the variable age.
+  public void empAge(int empAge){
+  age = emppAge;
+}
+
+/*Assign the designation to the variable designation.*/
+public void empDesignation(String empDesig) {
+	designation = empDesig;
+}
+
---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    var imgurl = previewPic.src;
+        var alttxt = previewPic.alt;
+   document.getElementById("image").innerHTML=previewPic.alt;
+
+document.getElementById("image").style.backgroundImage = "url("+previewPic.src+")";
+
+	}
+
+	function unDo(){
+         document.getElementById('image').style.backgroundImage="url('')"
+    document.getElementById('image').innerHTML = "Hover over an image below to display here";
+	}
+    
